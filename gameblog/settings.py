4077ee1997a6109bbe16681730b73593818a5b28
Renar <imperecdiego@gmail.com>
2020-03-27 20:55:14 +0300

    Fix media

--- gameblog/settings.py
+++ gameblog/settings.py
@@ -156,7 +156,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
 MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 
 EMAIL_HOST='smtp.gmail.com';
