92c0f215ead243268ed4c12735e14becfa143e82
RenarReineke <imperecdiego@gmail.com>
2020-03-31 20:32:46 +0300

    Уточнение настроке для Яндекс диска

--- gameblog/settings.py
+++ gameblog/settings.py
@@ -179,7 +179,7 @@ STATIC_URL ='/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
-MEDIA_URL='/media/'
+MEDIA_URL='https://disk.yandex.ru/client/recent'          #'/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 
