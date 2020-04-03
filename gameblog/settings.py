736ea46da5ac8d35d6496fb0ca3e4e31598c0da8
RenarReineke <imperecdiego@gmail.com>
2020-04-03 12:04:47 +0300

    Фикс 2 Хероку-Дропбокс

--- gameblog/settings.py
+++ gameblog/settings.py
@@ -101,23 +101,23 @@ WSGI_APPLICATION = 'gameblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'gameblog',
-        'USER':'postgres',
-        'PASSWORD':'Python777',
-#        'HOST':'127.0.0.1',
-        'PORT':'5432',
-    }
-}
-
 #DATABASES = {
  #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME':'sqlite3',
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME':'gameblog',
+    #    'USER':'postgres',
+     #   'PASSWORD':'Python777',
+#        'HOST':'127.0.0.1',
+      #  'PORT':'5432',
     #}
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':'sqlite3',
+    }
+}
 
 
 #db_from_env=dj_database_url.config()
